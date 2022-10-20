318d30216c81de393ae69f53d4e487ea1e9dd556
endlmk <endlmk@users.noreply.github.com>
2022-10-20 21:05:49 +0900

    Refactor Straight.

--- TDD-Poker-fsharp/TwoHandPoker.fs
+++ TDD-Poker-fsharp/TwoHandPoker.fs
@@ -11,10 +11,15 @@ let isHighCard a b =
 
 
 let isStraight (a: Card.Card)  (b: Card.Card) =
-    let rankOrder = [| "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; "J"; "Q"; "K"; "A"|]
-    let index_a = Array.findIndex (fun x -> x = a.rank) rankOrder
-    let index_b = Array.findIndex (fun x -> x = b.rank) rankOrder
-    abs (index_a - index_b) = 1 || (index_a = 12 && index_b = 0) || (index_a = 0 && index_b = 12)
+    let rankOrder = [| "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; "J"; "Q"; "K"; "A" |]
+    let index_a = Array.findIndex ((=) a.rank) rankOrder
+    let index_b = Array.findIndex ((=) b.rank) rankOrder
+    
+    let minDistanceOnCircular a b lengthCircular =
+        let l1 = (abs (a - b))
+        min l1 (lengthCircular - l1)
+    
+    minDistanceOnCircular index_a index_b rankOrder.Length = 1
     
 let isStraightFlush a b =
     (isStraight a b) && (isFlush a b)
