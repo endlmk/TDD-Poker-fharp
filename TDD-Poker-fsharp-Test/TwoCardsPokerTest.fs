79e25c834dd7b9d680e35d264b0d9ab4676268d4
endlmk <endlmk@users.noreply.github.com>
2022-10-22 23:23:04 +0900

    Impl two card poker judge.

--- TDD-Poker-fsharp-Test/TwoCardsPokerTest.fs
+++ TDD-Poker-fsharp-Test/TwoCardsPokerTest.fs
@@ -30,36 +30,115 @@ let judgeHandsTest() =
 
 [<Test>]
 let judgeResultWhenHandsDiffer() =
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")]) // StraightFlush
-                        (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
-        Assert.AreEqual(TwoHandPoker.Win, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
-                        (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
-        Assert.AreEqual(TwoHandPoker.Win, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
-                        (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
-        Assert.AreEqual(TwoHandPoker.Win, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
-                        (TwoHandPoker.createCards [("♥", "4"); ("♠", "6")]) // HighCard
-        Assert.AreEqual(TwoHandPoker.Win, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♥", "4"); ("♠", "6")]) // HighCard
-                        (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
-        Assert.AreEqual(TwoHandPoker.Lose, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
-                        (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
-        Assert.AreEqual(TwoHandPoker.Lose, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
-                        (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
-        Assert.AreEqual(TwoHandPoker.Lose, result)
-        let result = TwoHandPoker.judge
-                        (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
-                        (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")]) // StraightFlush
-        Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")]) // StraightFlush
+                    (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
+                    (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
+                    (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
+                    (TwoHandPoker.createCards [("♥", "4"); ("♠", "6")]) // HighCard
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♥", "4"); ("♠", "6")]) // HighCard
+                    (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")]) // Flush
+                    (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "2"); ("♥", "A")]) // Straight
+                    (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♥", "A")]) // Pair
+                    (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")]) // StraightFlush
+    Assert.AreEqual(TwoHandPoker.Lose, result)
     
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")]) // StraightFlush
+                    (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")]) // StraightFlush
+    Assert.AreEqual(TwoHandPoker.Draw, result)
+
+[<Test>]
+let judgeResultWhenHandsAreStraightFlush() =
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♦", "2")])
+                    (TwoHandPoker.createCards [("♥", "A"); ("♥", "K")])
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "3"); ("♦", "2")])
+                    (TwoHandPoker.createCards [("♥", "3"); ("♥", "4")])
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "K"); ("♦", "Q")])
+                    (TwoHandPoker.createCards [("♥", "Q"); ("♥", "J")])
+    Assert.AreEqual(TwoHandPoker.Win, result)
+
+[<Test>]
+let judgeResultWhenHandsArePair() =
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♠", "A")])
+                    (TwoHandPoker.createCards [("♥", "K"); ("♣", "K")])
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "2"); ("♠", "2")])
+                    (TwoHandPoker.createCards [("♥", "A"); ("♣", "A")])
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "J"); ("♠", "J")])
+                    (TwoHandPoker.createCards [("♥", "J"); ("♣", "J")])
+    Assert.AreEqual(TwoHandPoker.Draw, result)
+        
+[<Test>]
+let judgeResultWhenHandsAreStraight() =
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♠", "2")])
+                    (TwoHandPoker.createCards [("♥", "K"); ("♣", "A")])
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "6"); ("♠", "7")])
+                    (TwoHandPoker.createCards [("♥", "5"); ("♣", "6")])
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♠", "2")])
+                    (TwoHandPoker.createCards [("♥", "2"); ("♣", "A")])
+    Assert.AreEqual(TwoHandPoker.Draw, result)
+
+[<Test>]
+let judgeResultWhenHandsAreFlush() =
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "A"); ("♦", "3")])
+                    (TwoHandPoker.createCards [("♣", "4"); ("♣", "A")])
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "5"); ("♦", "7")])
+                    (TwoHandPoker.createCards [("♣", "6"); ("♣", "4")])
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "6"); ("♦", "4")])
+                    (TwoHandPoker.createCards [("♥", "4"); ("♥", "6")])
+    Assert.AreEqual(TwoHandPoker.Draw, result)
+
+[<Test>]
+let judgeResultWhenHandsAreHighCard() =
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "K"); ("♥", "10")])
+                    (TwoHandPoker.createCards [("♣", "J"); ("♥", "K")])
+    Assert.AreEqual(TwoHandPoker.Lose, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♥", "9"); ("♦", "2")])
+                    (TwoHandPoker.createCards [("♥", "8"); ("♣", "6")])
+    Assert.AreEqual(TwoHandPoker.Win, result)
+    let result = TwoHandPoker.judge
+                    (TwoHandPoker.createCards [("♦", "6"); ("♣", "4")])
+                    (TwoHandPoker.createCards [("♥", "4"); ("♣", "6")])
+    Assert.AreEqual(TwoHandPoker.Draw, result)
--- TDD-Poker-fsharp/TwoHandPoker.fs
+++ TDD-Poker-fsharp/TwoHandPoker.fs
@@ -6,6 +6,10 @@ let createCards suitRankTuples =
     let cards = suitRankTuples |> Seq.map (fun (s, r) -> Card.create s r) |> Seq.toArray
     { cards = cards }
 
+let rankOrder = [| "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; "J"; "Q"; "K"; "A" |]
+
+let rankValue rank = Array.findIndex ((=) rank) rankOrder
+
 let isCardsPair a b =
     Card.hasSameRank a b
 
@@ -19,9 +23,8 @@ let isFlush cards =
     isCardsFlush cards.cards[0] cards.cards[1]
 
 let isCardsStraight (a: Card.Card)  (b: Card.Card) =
-    let rankOrder = [| "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; "J"; "Q"; "K"; "A" |]
-    let index_a = Array.findIndex ((=) a.rank) rankOrder
-    let index_b = Array.findIndex ((=) b.rank) rankOrder
+    let index_a = rankValue a.rank
+    let index_b = rankValue b.rank
     
     let minDistanceOnCircular a b lengthCircular =
         let l1 = (abs (a - b))
@@ -42,14 +45,33 @@ type Result =
     Win | Lose | Draw
 type Hands =
     HighCard | Flush | Straight | Pair | StraightFlush
+
+type HandsWithRank = { hands: Hands; rank: int array; }
+
+let rankOfStraight cards =
+    let rank1 = rankValue cards.cards[0].rank
+    let rank2 = rankValue cards.cards[1].rank
+    let sorted = Array.sort [| rank1; rank2;|]
     
+    if sorted[0] = 0 && sorted[1] = 12 then [| 0 |]
+    else [| sorted[1] |]
+
+let rankOfPair cards =
+    let rank = rankValue cards.cards[0].rank
+    [| rank |]
+
+let rankOfFlushOrHighCard cards =
+    let rank1 = rankValue cards.cards[0].rank
+    let rank2 = rankValue cards.cards[1].rank
+    Array.sortDescending [| rank1; rank2;|]
+
 let judgeHands cards =
-    if isStraightFlush cards then StraightFlush
-    elif isPair cards then Pair
-    elif isStraight cards then Straight
-    elif isFlush cards then Flush
-    else HighCard
-    
+    if isStraightFlush cards then { hands = StraightFlush; rank = rankOfStraight cards; }
+    elif isPair cards then { hands = Pair; rank = rankOfPair cards; }
+    elif isStraight cards then { hands = Straight; rank = rankOfStraight cards; }
+    elif isFlush cards then { hands = Flush; rank = rankOfFlushOrHighCard cards; }
+    else { hands = HighCard; rank = rankOfFlushOrHighCard cards; }
+
 let judge myCards opponentCards =
     let myHands = judgeHands myCards
     let opponentHands = judgeHands opponentCards
@@ -57,3 +79,4 @@ let judge myCards opponentCards =
     if myHands > opponentHands then Win
     elif myHands < opponentHands then Lose
     else Draw
+
